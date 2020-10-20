4103e5bd6a2588427eb1db0f5286741b1cf4529c
AhadKhan98 <ahadkhan98@hotmail.com>
2020-10-20 10:47:11 +0500

    add first person camera and basic movement

---
+++ game/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 10f;
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate amount to move based on input
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+
+        // Final movement vector
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(velocity);
+    }
+
+}

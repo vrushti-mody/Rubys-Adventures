b774742af07d881ca516a7541bdb05ccd850be12
AhadKhan98 <ahad_zai@berea.edu>
2020-10-27 16:40:30 +0500

    Add back button for returning to main menu on both games

---
+++ Games/Forest of Terrors/Assets/CanvasHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasHandler : MonoBehaviour
+{
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
